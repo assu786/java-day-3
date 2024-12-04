b1001f3c9069d0dd4c4803efee673959ab3a9e7d
assu786 <shaikaqsa.78@gmail.com>
2024-12-04 14:36:09 +0530

    Add files via upload

---
+++ pow4.java
@@ -0,0 +1,16 @@
+class Bi
+{
+    public static void main(String args[])
+    {
+        int x=64;
+        if((x&(x-2))==0)
+        {
+            System.out.println("it is a power of 4");
+        }
+        else
+        {
+            System.out.println("it is not a power of 4");
+        }
+
+    }
+}
