15a0e50b51dcd0f38e2af87d198bd8adf9951211
assu786 <shaikaqsa.78@gmail.com>
2024-12-04 13:52:22 +0530

    Add files via upload

---
+++ mulof4.java
@@ -0,0 +1,17 @@
+class Bel
+{
+    public static void main(String args[])
+    {
+        int a=10;
+        if((a&3)==0)
+        {
+            System.out.println("it is a multiple of 4");
+
+        }
+        else
+        {
+            System.out.println("it is not a multiple of 4");
+
+        }
+    }
+}
