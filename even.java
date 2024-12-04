02c84af583a1313708e8cf5e711963ffa0e1bd0c
assu786 <shaikaqsa.78@gmail.com>
2024-12-04 11:41:11 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,16 @@
+class Pos
+{
+    public static void main(String args[])
+    {
+        int a=10;
+        if ((a&1)==0)
+        {
+            System.out.println("even");
+        }    
+        else
+        {
+        System.out.println("odd");
+        }
+         
+    }
+}
