e208ad44dc83ff8742339a333e113997dc3f3696
assu786 <shaikaqsa.78@gmail.com>
2024-12-04 12:29:51 +0530

    Add files via upload

---
+++ bignumber.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Hel
+{
+    public static void main(String args[])
+    {
+        Scanner n=new Scanner(System.in);
+        int a=n.nextInt();
+        int b=n.nextInt();
+        if (a>b)
+        {
+            System.out.println("a is largest");
+        }
+        else
+        {
+            System.out.println("b is largest");
+        }
+    }
+}
