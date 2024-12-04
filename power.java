a7f09f170e2bb76523c8115d7f43d6032bcf63e7
assu786 <shaikaqsa.78@gmail.com>
2024-12-04 11:46:22 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,16 @@
+class  Pow
+{
+    public static void main(String args[])
+    {
+        int a=8;
+        if ((a&(a-1))==0)
+        {
+            System.out.println("yes,it is power of 2");
+        }
+        else
+        {
+            System.out.println("no,it is not a power of 2");
+
+        }
+    }
+}
