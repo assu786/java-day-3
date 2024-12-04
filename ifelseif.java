ac45c269ea80c30cb1b1fe6472cedaec06263b38
assu786 <shaikaqsa.78@gmail.com>
2024-12-04 14:13:16 +0530

    Add files via upload

---
+++ ifelseif.java
@@ -0,0 +1,19 @@
+class Aw
+{
+    public static void main(String args[])
+    {
+        int a=7,b=3,c=9;
+        if((a>b)&&(a>c))
+        {
+            System.out.println("a is largest");
+        }
+        else if((b>a)&&(b>c))
+        {
+            System.out.println("b is largest");
+        }
+        else 
+        {
+             System.out.println("c is largest");
+        }    
+    }
+}
